b49a1a45ba8bcc769fec92aa7cad3f79d2833dba
Willian Leone <willian.le@trampolimapp.com.br>
2019-08-08 15:00:57 -0300

    Fix bug configuration of android with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+
+  // Firestore.instance.collection("teste").document("test").setData({"teste" : "teste"});
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
